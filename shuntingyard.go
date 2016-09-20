71238b4c56cb36c61b43a126028657ebf27a3744
patrickrand <prand0001@gmail.com>
2016-09-20 08:44:47 -0400

    started trying to fix shuntingyard... wikipedia lied

--- shuntingyard.go
+++ shuntingyard.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 	"text/scanner"
@@ -13,8 +14,38 @@ var (
 	ErrExtraParenths       = errors.New("extra parenths token in expression stack")
 )
 
+func push(stack []string, s string) []string {
+	stack = append(stack, s)
+	fmt.Printf("push %s\t\t= %v\n", s, stack)
+	return stack
+}
+
+func peek(stack []string) string {
+	if len(stack) == 0 {
+		return ""
+	}
+	return stack[0]
+}
+func pop(stack []string) (string, []string) {
+	n := len(stack)
+	if n == 0 {
+		return "", stack
+	}
+	top := stack[n-1]
+	stack = stack[:n-1]
+	fmt.Printf("pop %s\t\t= %v\n", top, stack)
+	return top, stack
+}
+
+func enqueue(sb *bytes.Buffer, elem string) {
+	sb.WriteString(elem)
+	fmt.Printf("enqueue %s\t= [%s]\n", elem, sb.String())
+	sb.WriteByte(' ')
+}
+
 // ConvertInfixToPostfix returns the postfix equivalent of the given infix expression.
 func ConvertInfixToPostfix(infix string) (postfix string, err error) {
+	fmt.Println("ConvertInfixToPostfix")
 	scan := new(scanner.Scanner).Init(strings.NewReader(infix))
 	scan.Mode = scanner.ScanIdents | scanner.ScanFloats
 
@@ -24,7 +55,6 @@ func ConvertInfixToPostfix(infix string) (postfix string, err error) {
 	}()
 
 	var stack []string
-	var prev string
 
 	for tok := scan.Scan(); tok != scanner.EOF; tok = scan.Scan() {
 		token := scan.TokenText()
@@ -32,36 +62,60 @@ func ConvertInfixToPostfix(infix string) (postfix string, err error) {
 
 		switch tokenType {
 		case NumberToken:
-			sb.WriteString(token)
-			sb.WriteByte(' ')
+			// if token == "-" {
+			// 	if tok = scan.Scan(); tok == scanner.EOF {
+			// 		return postfix, ErrInvalidStackOrdering
+			// 	}
+
+			// 	next := scan.TokenText()
+			// 	if _, err := strconv.ParseFloat(next, 0); err != nil {
+			// 		return postfix, ErrInvalidStackOrdering
+			// 	}
+
+			// 	if len(stack) == 0 || !isNumber(prev) {
+			// 		sb.WriteString(token)
+			// 		sb.WriteString(next)
+			// 	} else {
+			// 		sb.WriteString(next)
+			// 		sb.WriteByte(' ')
+			// 		sb.WriteString(token)
+			// 	}
+			// 	sb.WriteByte(' ')
+
+			// 	token = next
+			// 	break
+			// }
+			enqueue(sb, token)
 		case OperatorToken:
-			if token != "-" {
-				stack = append(stack, token)
+			if len(stack) == 0 {
+				stack = push(stack, token)
 				break
 			}
 
-			if tok = scan.Scan(); tok == scanner.EOF {
-				return postfix, ErrInvalidStackOrdering
+			top := stack[0]
+			p1, p2 := precedence(token), precedence(top)
+
+			if (p1 > p2) || (p1 == p2 && isRightAssociativeOperator(token)) {
+				stack = push(stack, token)
+				break
 			}
 
-			next := scan.TokenText()
-			if _, err := strconv.ParseFloat(next, 0); err != nil {
-				return postfix, ErrInvalidStackOrdering
-			}
+			//	var enqueued bool
+			for (p1 < p2) || (p1 == p2) && isLeftAssociativeOperator(token) {
+				enqueue(sb, top)
+				//enqueued = true
 
-			if len(stack) == 0 || !isNumber(prev) {
-				sb.WriteString(token)
-				sb.WriteString(next)
-			} else {
-				sb.WriteString(next)
-				sb.WriteByte(' ')
-				sb.WriteString(token)
+				top, stack = pop(stack)
+				if len(stack) == 0 {
+					break
+				}
+				p2 = precedence(top)
 			}
-			sb.WriteByte(' ')
-
-			token = next
+			// if !enqueued {
+			stack = push(stack, token)
+			// }
 		case FunctionToken, LeftParenthsToken:
-			stack = append(stack, token)
+			stack = push(stack, token)
 		case CommaToken, RightParenthsToken:
 			var i int
 			for i = len(stack) - 1; i >= 0; i-- {
@@ -69,8 +123,7 @@ func ConvertInfixToPostfix(infix string) (postfix string, err error) {
 				if elem == "(" {
 					break
 				}
-				sb.WriteString(elem)
-				sb.WriteByte(' ')
+				enqueue(sb, elem)
 			}
 
 			if i == -1 {
@@ -78,35 +131,37 @@ func ConvertInfixToPostfix(infix string) (postfix string, err error) {
 			}
 
 			if tokenType == CommaToken {
-				stack = stack[:i+1]
+				for j := 0; j < i+1; j++ {
+					pop(stack)
+				}
 				break
 			}
 
 			stack = stack[:i]
-
-			if len(stack) != 0 {
-				elem := stack[len(stack)-1]
-				if _, ok := Function(elem); ok {
-					stack = stack[:len(stack)-1]
-					sb.WriteString(elem)
-					sb.WriteByte(' ')
-				}
+			elem := peek(stack)
+			if _, ok := Function(elem); ok {
+				_, stack = pop(stack)
+				enqueue(sb, elem)
 			}
+			// if len(stack) != 0 {
+			// 	elem := stack[len(stack)-1]
+			// 	if _, ok := Function(elem); ok {
+			// 		stack = stack[:len(stack)-1]
+			// 		enqueue(sb, elem)
+			// 	}
+			// }
 		default:
 			return postfix, ErrInvalidPostfixToken
 		}
-
-		prev = token
 	}
 
+	var top string
 	for len(stack) != 0 {
-		elem := stack[len(stack)-1]
-		if elem == "(" || elem == ")" {
+		top, stack = pop(stack)
+		if top == "(" || top == ")" {
 			return postfix, ErrExtraParenths
 		}
-		stack = stack[:len(stack)-1]
-		sb.WriteString(elem)
-		sb.WriteByte(' ')
+		enqueue(sb, top)
 	}
 
 	return postfix, nil
